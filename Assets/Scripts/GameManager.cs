e97dc077fd5ca68edd250d22314be4721294c4dc
Nier07 <99447714+Nier07@users.noreply.github.com>
2022-02-18 18:10:27 +1100

    Added Raycasting and Crosshair

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static Vector2 scr;
+    private float pixelsPerUnit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scr.x = Screen.width / 16;
+        scr.y = Screen.height / 9;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.width/16 != scr.x)
+        {
+            scr.x = Screen.width / 16;
+            scr.y = Screen.height / 9;
+        } 
+    }
+}
--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -2,17 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[AddComponentMenu("RPG Game/Character/Interact")]
 public class Interact : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        //if a interact key is pressed
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //create ray shooting forward from the camera
+            Ray interact;
+            //assigning origin at center of screem
+            interact = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            //create hit info
+            RaycastHit hitInfo;
+            //if physics raycasts hits somthing within 10 units
+            if (Physics.Raycast(interact, out hitInfo, 10))
+            {
+                #region NPC tag
+                //raycast hits info tagged NPC
+                if (hitInfo.collider.CompareTag("NPC"))
+                {
+                    Debug.Log("NPC");
+                }
+                #endregion
+
+                #region Item
+                //raycast hits info tagged Item
+                if (hitInfo.collider.CompareTag("Item"))
+                {
+                    Debug.Log("Item");
+                    #endregion
+                }
+
+                #region Chest
+                //raycast hits info tagged Chest
+                if (hitInfo.collider.CompareTag("Chest"))
+                {
+                    Debug.Log("Chest");
+                }
+                #endregion
+            }
+        }
+    }
+    private void OnGUI()
+    {
+        //Interates through all x and y
+        for (int x = 0; x < 16; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                GUI.Box(new Rect(GameManager.scr.x * x, GameManager.scr.y * y,
+                    GameManager.scr.x, GameManager.scr.y), x+":"+y);
+            }
+        }
+        GUI.Box(new Rect(GameManager.scr.x * (8f - 0.125f), GameManager.scr.y * (4.5f - 0.125f), 
+            GameManager.scr.x * .25f, GameManager.scr.y * .25f), "");
     }
 }
